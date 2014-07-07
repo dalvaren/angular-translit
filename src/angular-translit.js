80bed0f62fbe246b717bae03d351dba54448ca26
Daniel Campos <daniel.campos@clickbus.com.br>
2014-07-07 09:54:09 -0300

    validate empty input

--- src/angular-translit.js
+++ src/angular-translit.js
@@ -3,6 +3,9 @@
 angular.module('translit',[])
 	.filter("translit", function(){
     return function (text, translitMap) {
+    	if (text === undefined || text == '') {
+    		return '';
+    	}
         var re = new RegExp(Object.keys(translitMap).join("|"),"gi");
         return text.replace(re, function(matched){
           return translitMap[matched];
