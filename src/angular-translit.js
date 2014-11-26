eae4f19de5224b32c198e371065defaf33cb7626
Daniel Campos <daniel.campos@clickbus.com.br>
2014-11-26 15:47:22 -0200

    fix undefined chars when no char is matched

--- src/angular-translit.js
+++ src/angular-translit.js
@@ -1,14 +1,17 @@
 'use strict';
 
 angular.module('translit',[])
-	.filter("translit", function(){
+    .filter("translit", function(){
     return function (text, translitMap) {
-    	if (text === undefined || text == '') {
-    		return '';
-    	}
+        if (typeof text === 'undefined' || text === '') {
+            return '';
+        }
         var re = new RegExp(Object.keys(translitMap).join("|"),"gi");
         return text.replace(re, function(matched){
+            if(typeof translitMap[matched] === 'undefined') {
+                return matched;
+            }
           return translitMap[matched];
         });
-    }
+    };
 });
